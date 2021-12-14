8cdb0711eb2fc98f21ce8320a5958c280f6f5eb3
ApollosSevere <severeapollos@gmail.com>
2021-12-13 21:19:51 -0500

    edit about photo

--- client/components/About/About.jsx
+++ client/components/About/About.jsx
@@ -5,7 +5,8 @@ function About() {
   return (
     <div className="about">
       <div className="about-left">
-        {/* <div className="about-card-bg"></div> */}
+        <div className="about-card-bg"></div>
+
         <div className="about-card">
           <div className="about-card-wrapper">
             <img
